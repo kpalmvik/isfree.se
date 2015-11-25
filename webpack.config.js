0b560bb95e9678879493d33b87b50cfedb1d1ce7
Kristofer Palmvik <kristofer@palmvik.se>
2015-11-25 08:52:20 +0100

    Use JSON loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,30 +2,31 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                test: path.join(__dirname, 'es6'),
-                query: {
-                  presets: 'es2015',
-                },
-            }
-        ]
-    },
-    plugins: [
-        // Avoid publishing files when compilation fails
-        new webpack.NoErrorsPlugin()
-    ],
-    stats: {
-        // Nice colored output
-        colors: true
-    },
-    // Create Sourcemaps for the bundle
-    devtool: 'source-map',
+  entry: './src/main.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      loader: 'babel-loader',
+      test: path.join(__dirname, 'es6'),
+      query: {
+        presets: 'es2015',
+      },
+    }, {
+      loader: 'json-loader',
+      test: /\.json$/
+    }]
+  },
+  plugins: [
+    // Avoid publishing files when compilation fails
+    new webpack.NoErrorsPlugin()
+  ],
+  stats: {
+    // Nice colored output
+    colors: true
+  },
+  // Create Sourcemaps for the bundle
+  devtool: 'source-map',
 };
