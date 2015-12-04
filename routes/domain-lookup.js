7008a086081bb05c36ea016323cbf6fed4936dee
Kristofer Palmvik <kristofer@palmvik.se>
2015-12-04 07:15:38 +0100

    Refactor domain parsing into separate method

--- routes/domain-lookup.js
+++ routes/domain-lookup.js
@@ -2,9 +2,13 @@ const router = require('express').Router();
 
 /* GET users listing. */
 router.get('/*', (req, res, next) => {
-  const urlPath = req.originalUrl;
-  const domainName = urlPath.split('/')[1];
+  const domainName = parseDomainFromUrlPath(req.originalUrl);
+
   res.send('Looking up ' + domainName);
 });
+
+function parseDomainFromUrlPath(urlPath) {
+  return urlPath.split('/')[1];
+}
 
 module.exports = router;
