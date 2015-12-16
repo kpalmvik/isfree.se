05fdcac6fa83207956f67bc3fa7b0ad8270543d0
Kristofer Palmvik <kristofer@palmvik.se>
2015-12-16 17:06:56 +0100

    Add .se using redirect if neccessary

--- routes/domain-lookup.js
+++ routes/domain-lookup.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 
 router.get('/*', (req, res, next) => {
   const seFree = require('se-free'),
-        domainName = parseDomainFromUrlPath(req.originalUrl),
-        domainLookup = seFree(domainName);
+        domainName = parseDomainFromUrlPath(req.originalUrl);
 
-  domainLookup.then(
-    (domainLookupResult) => {
+  if(!endsWithSe(domainName)) {
+    res.redirect(301, '/' + domainName + '.se');
+  };
+
+  seFree(domainName)
+    .then((domainLookupResult) => {
       res.render('domain-lookup', {
         domain: domainName,
         result: domainLookupResult});
@@ -16,6 +19,10 @@ router.get('/*', (req, res, next) => {
 
 function parseDomainFromUrlPath(urlPath) {
   return urlPath.split('/')[1];
+}
+
+function endsWithSe(domainName) {
+  return (domainName.substring(domainName.length - 3) === '.se');
 }
 
 module.exports = router;
