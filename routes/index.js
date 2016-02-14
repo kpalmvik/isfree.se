351e469ca447f5fb8334dd5cfd689827a6ea2099
Kristofer Palmvik <kristofer@palmvik.se>
2016-02-14 01:38:20 +0100

    Redesign index page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'isfree.se' });
+  res.render('index', {title: 'Isfree.se | Snabbkoll på lediga svenska .se-domäner' });
 });
 
 module.exports = router;
