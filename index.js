68dcf70b16cc32323739136b13d601a0c022a13b
Kristofer Palmvik <kristofer@palmvik.se>
2019-05-26 20:10:52 +0200

    Reset to hello world with correct dependencies

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-// Use babel-register to enable ES2015 in Node
-require('babel-register');
-// Start the express sever
-require('./bin/www');
+import express from 'express';
+import path from 'path';
+import helmet from 'helmet';
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// add some security-related headers to the response
+app.use(helmet());
+
+app.get('/', (req, res, next) => {
+  res.send('Hello world!');
+});
+
+export default app;
