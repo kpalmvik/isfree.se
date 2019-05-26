7d6e40749e3be9a8e35bc87968fa1073aa78e21c
Kristofer Palmvik <kristofer@palmvik.se>
2019-05-26 20:45:44 +0200

    Autofix Eslint warnings

--- routes/domainLookup.js
+++ routes/domainLookup.js
@@ -2,19 +2,19 @@ const router = require('express').Router();
 
 router.get('/*', (req, res, next) => {
   const seFree = require('se-free'),
-        domainName = parseDomainFromUrlPath(req.originalUrl);
+    domainName = parseDomainFromUrlPath(req.originalUrl);
 
-  if(!endsWithSe(domainName)) {
-    return res.redirect(301, '/' + domainName + '.se');
-  };
+  if (!endsWithSe(domainName)) {
+    return res.redirect(301, `/${domainName}.se`);
+  }
 
   seFree(domainName)
     .then((domainLookupResult) => {
       res.render('domain-lookup', {
         domain: domainName,
-        result: domainLookupResult});
-    }
-  );
+        result: domainLookupResult,
+      });
+    });
 });
 
 function parseDomainFromUrlPath(urlPath) {
