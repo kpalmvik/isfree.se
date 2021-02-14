675a82e91fba74e100570290793be9b5b62d5154
Kristofer Palmvik <kristofer@palmvik.se>
2021-02-14 21:05:44 +0100

    Add GA to head using React Helmet

--- components/Layout.js
+++ components/Layout.js
@@ -4,12 +4,25 @@ const Layout = ({
   pageTitleSuffix = 'Kolla snabbt om en svensk .se-domän är ledig!',
   children,
 }) => (
-  <div className="content">
-    <Head>
-      <title>{`isfree.se | ${pageTitleSuffix}`}</title>
-    </Head>
-    {children}
-  </div>
+  <>
+    <div className="content">
+      <Head>
+        <title>{`isfree.se | ${pageTitleSuffix}`}</title>
+        <script>
+          {`
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+          `}
+        </script>
+        <script
+          src="https://www.google-analytics.com/analytics.js"
+          async
+          defer
+        />
+      </Head>
+      {children}
+    </div>
+  </>
 );
 
 export default Layout;
