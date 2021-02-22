fa0c71c9aba1065abb198efb1fcaba4e591975ed
Kristofer Palmvik <kristofer@palmvik.se>
2021-02-22 21:33:42 +0100

    Change link to example without .se

--- components/Instructions.jsx
+++ components/Instructions.jsx
@@ -1,8 +1,7 @@
 const Instructions = () => (
   <>
-    Besök <a href="/example.se">isfree.se/example</a> för att snabbt se om
-    domänen <span className="url-nolink">example.se</span> är upptagen eller
-    ledig
+    Besök <a href="/example">isfree.se/example</a> för att snabbt se om domänen{' '}
+    <span className="url-nolink">example.se</span> är upptagen eller ledig
   </>
 );
 
