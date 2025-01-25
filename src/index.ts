70f12e3553d53ee861dcce3f5a0c7fe025d0769f
Kristofer Palmvik <kristofer@palmvik.se>
2025-01-25 12:40:14 +0100

    Add routes for domain lookup and redirect

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { Hono } from "hono";
+import { html } from "hono/html";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
+
+app.get("/:domain{.+(.se)$}", (c) => {
+  const domain = c.req.param("domain");
+  return c.html(
+    html`<!DOCTYPE html>
+      <h1>Looking up! ${domain}!</h1>`
+  );
+});
+
+app.get("/:domain{[^.]+}", (c) => c.redirect(`/${c.req.param("domain")}.se`));
 
 export default app;
