a7ec43cabba1ac49adee009007c1020efb134e3b
Kristofer Palmvik <kristofer@palmvik.se>
2025-01-25 11:59:27 +0100

    Remove old code

--- components/Instructions.jsx
+++
@@ -1,8 +0,0 @@
-const Instructions = () => (
-  <>
-    Besök <a href="/example">isfree.se/example</a> för att snabbt se om domänen{' '}
-    <span className="url-nolink">example.se</span> är upptagen eller ledig
-  </>
-);
-
-export default Instructions;
--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable react/prop-types */
-/* eslint-disable react/jsx-props-no-spreading */
-import '../styles/sanitize.css';
-import '../styles/style.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
