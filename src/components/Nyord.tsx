32b6ccd5c85f2753338f4efcd01d9fc2fead6e66
Kristofer Palmvik <kristofer@palmvik.se>
2025-12-29 08:17:32 +0100

    Display all nyord years on start page

--- src/components/Nyord.tsx
+++ src/components/Nyord.tsx
@@ -1,35 +1,58 @@
 import { nyord2022, nyord2023, nyord2024, nyord2025 } from "./nyord-data";
 
-const getRandomItem = <T,>(array: T[]): T => {
-  const randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-};
-
 const Nyord = () => {
-  const allNyordYears = [nyord2022, nyord2023, nyord2024, nyord2025];
-
-  const nyord = getRandomItem(allNyordYears);
+  const allNyordYears = [nyord2025, nyord2024, nyord2023, nyord2022];
 
   return (
-    <>
-      <h2 className="example__title">Nyord {nyord.year}</h2>
-      <p className="example__text">
-        Kolla om något av orden från{" "}
-        <a href={nyord.link} target="_blank" rel="noopener" className="url">
-          nyordslistan&nbsp;{nyord.year}
-        </a>{" "}
-        är lediga som domännamn.
-      </p>
-      <ul className="example__list example__list--columns">
-        {nyord.words.map((word) => (
-          <li className="example__list-item" key={word}>
-            <a href={`/${word}`} className="url">
-              {word}.se
-            </a>
+    <ul class="nyord-years">
+      {allNyordYears.map(({ year, link, words }, i) => {
+        const previousYear = allNyordYears[i + 1]?.year;
+        const nextYear = allNyordYears[i - 1]?.year;
+
+        return (
+          <li key={year} class="nyord-year" id={`nyord-${year}`}>
+            <div>
+              <h2 className="example__title">Nyord {year}</h2>
+              <p className="example__text">
+                Kolla om något av orden från{" "}
+                <a href={link} target="_blank" rel="noopener" className="url">
+                  nyordslistan&nbsp;{year}
+                </a>{" "}
+                är lediga som domännamn.
+              </p>
+              <ul className="example__list example__list--columns">
+                {words.map((word) => (
+                  <li className="example__list-item" key={word}>
+                    <a href={`/${word}`} className="url">
+                      {word}.se
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <ul className="nyord-nav">
+              <>
+                <li>
+                  {nextYear && (
+                    <a href={`#nyord-${nextYear}`} className="url">
+                      ← Nyord från {nextYear}
+                    </a>
+                  )}
+                </li>
+
+                <li>
+                  {previousYear && (
+                    <a href={`#nyord-${previousYear}`} className="url">
+                      Nyord från {previousYear} →
+                    </a>
+                  )}
+                </li>
+              </>
+            </ul>
           </li>
-        ))}
-      </ul>
-    </>
+        );
+      })}
+    </ul>
   );
 };
 
